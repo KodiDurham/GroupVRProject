0a7f4b24ea1d2624de39cb111d20def9b6a16155
KodiD <40518298+KodiD@users.noreply.github.com>
2020-11-18 17:39:51 -0500

    added some scripts to manage openning a door

---
+++ Assets/doorManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorManager : MonoBehaviour
+{
+    public GameObject door; //or animator to call open
+
+
+   public bool[] hasKeys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void updateKey(int i, bool hasKey)
+    {
+        hasKeys[i] = hasKey;
+        testKeys();
+    }
+
+    public void testKeys()
+    {
+        bool hasAllKeys = true;
+        foreach(bool key in hasKeys)
+        {
+            if (key == false)
+            {
+                hasAllKeys = false;
+                break;
+            }
+        }
+
+        if (hasAllKeys)
+        {
+            openDoor();
+        }
+    }
+
+    public void openDoor()
+    {
+        Debug.Log("OPEN DOOR!!!");
+    }
+}
---
+++ Assets/objectEnterTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectEnterTrigger : MonoBehaviour
+{
+    public int keyIndex;
+
+    public GameObject key;
+
+    public bool hasKey;
+
+    public doorManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasKey = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject == key)
+        {
+            hasKey = true;
+            manager.updateKey(keyIndex,hasKey);
+            Debug.Log("key " + keyIndex + " in");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.gameObject == key)
+        {
+            hasKey = false;
+            manager.updateKey(keyIndex,hasKey);
+            Debug.Log("key " + keyIndex + " out");
+        }
+    }
+}
